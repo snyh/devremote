995cd0ed8cd367aec170e310fd1c4b98f286ab00
snyh <snyh@snyh.org>
2020-01-21 11:23:50 +0800

    implement devremote-switch-server

--- devremote.el
+++ devremote.el
@@ -1,4 +1,5 @@
-;;; devremote.el --- Write on home and test on remote server in Emacs.
+;;; devremote.el --- Make transfering code to remote server
+;;;      more easy for temporary development and debugging.
 ;;; Commentary:
 ;;    automatically transfer to remote server.
 ;;
@@ -6,12 +7,17 @@
 (require 'subr-x)
 (require 'dash)
 (require 'easymenu)
+(require 'projectile)
+(require 'pcmpl-unix)
+(require 'helm)
 
+;;;; code:
 (defvar devremote-mode-lighter " DR")
 
 (defvar devremote-default-server "/ssh:sw-sh")
 
 (defun error-not-in-project (file)
+  "FILE is not in project."
   (user-error "The file %s isn't in any project"
               file))
 
@@ -22,23 +28,35 @@
   remote-root-dir
   build-cmd)
 
-(setq devremote-project-infos
-      (list (make--pinfo
-             :server devremote-default-server
-             :local-root-dir "~/codes/node"
-             :ignore-dirs '(".git" "out")
-             :remote-root-dir "~/snyh/node")
-            (make--pinfo
-             :server devremote-default-server
-             :local-root-dir "~/codes/LuaJIT"
-             :ignore-dirs '(".git")
-             :remote-root-dir "~/snyh/LuaJIT")
-            (make--pinfo
-             :server devremote-default-server
-             :local-root-dir "~/codes/go-sw64"
-             :ignore-dirs '(".git" "pkg")
-             :remote-root-dir "~/snyh/go-sw64")
-            ))
+(setq-default devremote-project-infos nil)
+
+(defcustom devremote-known-projects-file
+  (expand-file-name "devremote.projects" user-emacs-directory)
+  "The name of devremote's known projects file."
+  :group 'devremote
+  :type 'string)
+
+(defun devremote-serialize (data filename)
+  "Serialize the DATA configure to FILENAME."
+  (with-temp-file filename
+    (insert (prin1-to-string data))))
+
+(defun devremote-unserialize (filename)
+  "Read data serialized by `devremote-serialize` from FILENAME."
+  (with-demoted-errors
+      "Error during devremote-unserialize: %S\n"
+    (when (file-exists-p filename)
+      (with-temp-buffer
+        (insert-file-contents filename)
+        (read (buffer-string)))
+      )))
+
+(defun devremote-save-known-projects ()
+  (devremote-serialize devremote-project-infos devremote-known-projects-file))
+
+(defun devremote-load-known-projects ()
+  (setq devremote-project-infos
+        (devremote-unserialize devremote-known-projects-file)))
 
 (defun -in-dir(f dir) (string-prefix-p dir f))
 (defun -not-in-dir (f dir) (not (-in-dir f dir)))
@@ -51,14 +69,16 @@
   "Check whether the PINFO contain FILE.
 
 FILE must in local root directory and must not in any of ignore directories."
-  (let ((_local-root (expand-file-name (-pinfo-local-root-dir pinfo)))
-        (_ignore-dirs (-pinfo-ignore-dirs pinfo))
-        (_file (expand-file-name file)))
-    (seq-reduce (lambda (acc elt) (and acc (-not-in-dir _file elt)))
-                (--expand-ignore-dirs _local-root _ignore-dirs)
-                (-in-dir _file _local-root))))
+  (if file
+      (let ((_local-root (expand-file-name (-pinfo-local-root-dir pinfo)))
+	    (_ignore-dirs (-pinfo-ignore-dirs pinfo))
+	    (_file (expand-file-name file)))
+	(seq-reduce (lambda (acc elt) (and acc (-not-in-dir _file elt)))
+		    (--expand-ignore-dirs _local-root _ignore-dirs)
+		    (-in-dir _file _local-root)))))
 
 (defun devremote-query-pinfo (file)
+  (interactive "bBuffer")
   (-first (lambda (pinfo) (--pinfo-contain-file pinfo file))
           devremote-project-infos))
 
@@ -99,10 +119,29 @@ FILE must in local root directory and must not in any of ignore directories."
     (unless pinfo (error-not-in-project buffer-file-name))
     (compilation-start (-build-rsync-cmd pinfo))))
 
+
+
+(setq ssh-helm-source
+      `((name . "Known hosts")
+        (candidates . ,(pcmpl-ssh-config-hosts))
+        (action . (lambda (candidate)
+                    (helm-marked-candidates)))))
+(defun devremote-switch-server (host)
+  "Request switch server to HOST."
+  (interactive
+   (or (helm :sources '(ssh-helm-source)) '(nil)))
+  (if host
+    (let* ((pinfo (devremote-query-pinfo buffer-file-name)))
+      (unless pinfo (error-not-in-project buffer-file-name))
+      (setf (-pinfo-server pinfo) (format "/ssh:%s" host))
+      (--devremote-update-lighter)
+      )))
+
+
 (defun devremote-compilation-project ()
   (interactive)
   (let ((pinfo (devremote-query-pinfo buffer-file-name)))
-    (unless pinfo (error-not-in-project long-name))
+    (unless pinfo (error-not-in-project buffer-file-name))
     (let* ((remote-root-dir (-pinfo-remote-root-dir pinfo))
            (local-root-dir (expand-file-name (-pinfo-local-root-dir pinfo)))
            (remote-server (string-remove-prefix "/ssh:" (-pinfo-server pinfo)))
@@ -163,6 +202,13 @@ FILE must in local root directory and must not in any of ignore directories."
                     (symbol-value
                      (make-local-variable 'devremote-mode-lighter)))))))
 
+(defun --devremote-update-lighter()
+  (if (-current-buffer-in-project)
+      (setq-local devremote-mode-lighter
+		  (let ((server (-pinfo-server (devremote-query-pinfo (buffer-file-name)))))
+		    (format " DR(%s)" (string-remove-prefix "/ssh:" server)))
+		  )))
+
 (defun --devremote-find-file-hook-function()
   (when (-current-buffer-in-project)
     (devremote-mode)
@@ -173,11 +219,9 @@ FILE must in local root directory and must not in any of ignore directories."
 (define-minor-mode devremote-mode
   "Write code on home and test them on server by ssh"
   :lighter devremote-mode-lighter
-  :keymap (let ((map (make-sparse-keymap "devremote")))
-            (define-key map (kbd "<f12>") 'devremote-transfer-current)
-            (define-key map (kbd "<f11>") 'devremote-transfer-project)
-            (define-key map (kbd "M-<f12>") 'devremote-compilation-project)
-            map)
+  :keymap (make-sparse-keymap "devremote")
+  :after-hook
+  (--devremote-update-lighter)
   )
 
 (defun devremote-create-project(local-root ignores server remote-root cmd)
@@ -198,7 +242,8 @@ FILE must in local root directory and must not in any of ignore directories."
                 :remote-root-dir remote-root
                 :build-cmd cmd)))
     (add-to-list 'devremote-project-infos pinfo)
-    (message "TODO Create %S" pinfo)))
+    (devremote-save-known-projects)
+    (--devremote-find-file-hook-function)))
 
 (easy-menu-define devremote-menu devremote-mode-map
   "Menus for devremote-mode."
@@ -207,6 +252,8 @@ FILE must in local root directory and must not in any of ignore directories."
     ["Transfer Current" devremote-transfer-current]
     ["Automatically Transfer" devremote-toggle-auto-transfer]
     ["Transfer Project" devremote-transfer-project]))
+
+(devremote-load-known-projects)
 
 (provide 'devremote)
 
